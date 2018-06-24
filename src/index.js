4f474192caf1bca0211def389f835ff250afe500
lx kish <urban.ktulhu@gmail.com>
2018-06-24 14:10:08 +1200

    24 creating routes

--- src/index.js
+++ src/index.js
@@ -1,1 +1,28 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 
+//COMPONENTS
+import Home from './components/home';
+import Posts from './components/posts';
+import Profile from './components/profiles';
+
+const App = () => {
+    return (
+        <BrowserRouter>
+            <div>
+                <header>
+                    Header
+                </header>
+                <Route path="/" exact component={Home}/>
+                <Route path="/posts" component={Posts} />
+                <Route path="/profile" component={Profile} />
+            </div>
+        </BrowserRouter>
+    )
+}
+
+ReactDOM.render(
+    <App />,
+    document.querySelector('#root')
+)
