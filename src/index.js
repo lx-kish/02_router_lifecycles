fcd33d61729e8a9db891da8fecb426b930dcf8ff
lx kish <urban.ktulhu@gmail.com>
2018-06-24 18:45:05 +1200

    27 Using Router's Switch

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Route, Link, NavLink } from 'react-router-dom';
+import { BrowserRouter, Route, Link, NavLink, Switch } from 'react-router-dom';
 
 //COMPONENTS
 import Home from './components/home';
@@ -13,20 +13,22 @@ const App = () => {
         <BrowserRouter>
             <div>
                 <header>
-                    <NavLink to="/">Home</NavLink><br/>
-                    <NavLink 
+                    <NavLink to="/">Home</NavLink><br />
+                    <NavLink
                         to="/posts"
-                        activeStyle={{color:'red'}}
+                        activeStyle={{ color: 'red' }}
                         activeClassName="selected"
-                    >Posts</NavLink><br/>
+                    >Posts</NavLink><br />
                     <NavLink to={{
-                        pathname:'/profile'
-                    }}>Profile</NavLink><br/>
+                        pathname: '/profile'
+                    }}>Profile</NavLink><br />
                 </header>
-                <Route path="/" exact component={Home}/>
-                <Route path="/posts" exact component={Posts} />
-                <Route path="/posts/:id/:username" component={PostItem} />
-                <Route path="/profile" component={Profile} />
+                <Switch>
+                    <Route path="/posts/:id/:username" component={PostItem} />
+                    <Route path="/posts" component={Posts} />
+                    <Route path="/profile" component={Profile} />
+                    <Route path="/" exact component={Home} />
+                </Switch>
             </div>
         </BrowserRouter>
     )
